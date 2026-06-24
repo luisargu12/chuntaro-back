d1ccef1d19f17e1d1e633dd8ce627333153e100b
Luis Arguello <luis.argu00@gmail.com>
2026-06-24 09:58:03 -0600

    Cambios en el login

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'], // Asegúrate de que 'login' esté aquí
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')], // Esto lee tu .env
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => true, // ¡ESTA ES LA LÍNEA MÁGICA!
 
 ];
